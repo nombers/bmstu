d7ab2c2f3dc0bc84e10c666d73154e275d5b18f5
nombers <konsentik@gmail.com>
2022-09-21 20:49:33 +0300

    Перемножение мтриц

---
+++ 16.09.2022/premnojenie_matrix.rb
@@ -0,0 +1,36 @@
+print "Введите сколько строк и столбцов 1 матрицы: "
+a, b = readline.split.map(&:to_i)
+
+arr1, arr2, arr3 = [], [], []
+
+for i in (1..a) 
+    stroka = readline.split.map(&:to_i)
+    arr1 += [stroka]
+end
+
+print "Введите сколько строк и столбцов 2 матрицы: "
+c, d = readline.split.map(&:to_i)
+
+for i in (1..c) 
+    stroka = readline.split.map(&:to_i)
+    arr2 += [stroka]
+end
+
+h = 0
+lol = []
+if b == c
+    for i in (0..a - 1) # количество строк 
+        for j in (0..d - 1) # количество стобцоов
+            for k in (0..b - 1) # пробежаться по строку первой матрицы и столбу второй матрицы
+                h += arr1[i][k] * arr2[k][j]
+            end
+            lol.append(h)
+            h = 0
+        end
+        arr3 += [lol]
+        lol = []
+    end
+    print arr3
+else
+    puts "Перемножить матрицы нельзя!!!"
+end
