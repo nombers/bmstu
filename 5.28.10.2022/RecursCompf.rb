2eb9342af669a38a5ea436e35028eae11370f2ed
nombers <konsentik@gmail.com>
2022-11-08 22:25:56 +0300

    all

--- 5.28.10.2022/RecursCompf.rb
+++ 5.28.10.2022/RecursCompf.rb
@@ -1,44 +1,43 @@
 class RecursComp
     def compile(str)
         @str, @index = str, 0
-        @str = @str.split().join()
         compileF
     end
     
     private
   
     def compileF
+        compileC
         compileT
         return if @index >= @str.length
         cur = @str[@index].chr 
         if cur == '+' or cur == '-'
             @index += 1
+            compileC
             compileF
             print "#{cur} "
         end
     end
     def compileT
+        compileC
         compileM
         return if @index >= @str.length
         cur = @str[@index].chr
         if cur == '*' or cur == '/' or cur == '%'
             @index += 1
+            compileC
             compileT
             print "#{cur} "
         end
     end
     def compileM
-        if @index + 1 <= @str.length and (@str[@index] + @str[@index + 1] == "/*" or @str[@index] + @str[@index + 1] == "//")
-            @index += 1
-            while @str[@index] + @str[@index + 1] != "*/" or @str[@index] + @str[@index + 1] != "//"
-                @index += 1
-            end
-            print("\n", @str[@index])
-        end
+        return if @index >= @str.length
         if @str[@index].chr == '(' or @str[@index].chr == '[' or @str[@index].chr == '{'
             @index += 1
+            compileC
             compileF
             @index += 1
+            compileC
         else
             compileV
         end
@@ -46,6 +45,24 @@ class RecursComp
     def compileV
         print "#{@str[@index].chr} "
         @index += 1
+        compileC
+    end
+    def complitP
+        if @str[@index] == " "
+            @index += 1
+        end
+    end
+    def compileC
+        if @index + 1 < @str.length 
+            if @str[@index] + @str[@index + 1] == "/*" or @str[@index] + @str[@index + 1] == "//"
+                @index += 1
+                while @str[@index] + @str[@index + 1] != "*/" and @str[@index] + @str[@index + 1] != "//"
+                    @index += 1
+                end
+                @index += 2
+            end
+        end
+        complitP
     end
 end
   
---
+++ 5.28.10.2022/test.rb
@@ -0,0 +1,3 @@
+a = "asda"
+d = a[1] + a[2]
+print(d)
