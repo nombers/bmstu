e17f4778ffcb851e83c51cfe94a24267ff37ae77
nombers <konsentik@gmail.com>
2022-11-08 21:03:22 +0300

    help

--- 5.28.10.2022/RecursCompf.rb
+++ 5.28.10.2022/RecursCompf.rb
@@ -1,50 +1,57 @@
 class RecursComp
-  def compile(str)
-    @str,@index = str,0
-    compileF
-  end
+    def compile(str)
+        @str, @index = str, 0
+        @str = @str.split().join()
+        compileF
+    end
+    
+    private
   
-  private
-
-  def compileF
-    compileT
-    return if @index >= @str.length
-    cur = @str[@index].chr
-    if cur == '+' or cur == '-'
-      @index += 1
-      compileF
-      print "#{cur} "
+    def compileF
+        compileT
+        return if @index >= @str.length
+        cur = @str[@index].chr 
+        if cur == '+' or cur == '-'
+            @index += 1
+            compileF
+            print "#{cur} "
+        end
     end
-  end
-  def compileT
-    compileM
-    return if @index >= @str.length
-    cur = @str[@index].chr
-    if cur == '*' or cur == '/'
-      @index += 1
-      compileT
-      print "#{cur} "
+    def compileT
+        compileM
+        return if @index >= @str.length
+        cur = @str[@index].chr
+        if cur == '*' or cur == '/' or cur == '%'
+            @index += 1
+            compileT
+            print "#{cur} "
+        end
     end
-  end
-  def compileM
-    if @str[@index].chr == '('
-      @index += 1
-      compileF
-      @index += 1
-    else
-      compileV
+    def compileM
+        if @index + 1 <= @str.length and (@str[@index] + @str[@index + 1] == "/*" or @str[@index] + @str[@index + 1] == "//")
+            @index += 1
+            while @str[@index] + @str[@index + 1] != "*/" or @str[@index] + @str[@index + 1] != "//"
+                @index += 1
+            end
+            print("\n", @str[@index])
+        end
+        if @str[@index].chr == '(' or @str[@index].chr == '[' or @str[@index].chr == '{'
+            @index += 1
+            compileF
+            @index += 1
+        else
+            compileV
+        end
     end
-  end
-  def compileV
-    print "#{@str[@index].chr} "
-    @index += 1
-  end
+    def compileV
+        print "#{@str[@index].chr} "
+        @index += 1
+    end
 end
-
+  
 c = RecursComp.new
 while true
-  print "Введите формулу: "
-  c.compile(readline.chomp)
-  print "\n"
+    print "Введите формулу: "
+    c.compile(readline.chomp)
+    print "\n"
 end
-
